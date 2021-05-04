cca29419638993a6f5817d59952ec4e33dafd387
Gersón Gómez <gergomez18@gmail.com>
2021-05-04 16:23:54 -0500

    Página de mapa

--- layouts/Index/map.php
+++ layouts/Index/map.php
@@ -1,3 +1,11 @@
 <?php defined('_EXEC') or die; ?>
 
-<main></main>
+<main>
+    <section class="p-20">
+        <div class="container">
+            <figure id="map" style="padding-top:250px;">
+                <img src="{$path.images}map.svg" alt="Map" class="img-fluid">
+            </figure>
+        </div>
+    </section>
+</main>
